b5b025934b091e176bf5c71e517883b3745a7db6
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-10-02 21:38:30 +0530

    Refactor TaskCard component to update UI styling and improve date formatting

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,21 +1,19 @@
 import React from "react";
 
 interface TaskCardProps {
-  title: string,
-  dueDate?: Date,
-  completedAtDate?: Date,
-  assigneeName: string,
+  title: string;
+  dueDate?: Date;
+  completedAtDate?: Date;
+  assigneeName: string;
 }
 
-const formateDate = (date: Date) => date.toLocaleDateString("en-CA")
-
-const TaskCard: React.FC<TaskCardProps> = ({ title , dueDate , completedAtDate ,assigneeName }) => {
+const TaskCard: React.FC<TaskCardProps> = ({ title, dueDate, completedAtDate, assigneeName }) => {
   return (
-    <div className="bg-cyan-400 shadow-lg p-4 rounded-md space-y-4">
-      <h2 className="text-2xl font-bold">{title}</h2>
-      <p> {dueDate  && <p>Due Date: {formateDate(dueDate)}</p>}</p>
-      <p>{completedAtDate && <p>Completed At: {formateDate(completedAtDate)}</p>}</p>
-      <p>Assignee: {assigneeName}</p>  
+    <div className="p-4 bg-cyan-400 rounded-md shadow-md">
+      <h3 className="text-lg font-bold">{title}</h3>
+      {dueDate && <p>Due on: {dueDate.toLocaleDateString("en-CA")}</p>}
+      {completedAtDate && <p>Completed on: {completedAtDate.toLocaleDateString("en-CA")}</p>}
+      <p>Assignee: {assigneeName}</p>
     </div>
   );
 };
