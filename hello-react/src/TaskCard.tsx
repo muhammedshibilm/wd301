e34e9ebc7b767881a5842def2ca8cf6a358b1dc0
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-10-09 20:48:59 +0530

    Refactor TaskCard component to update UI styling and improve date formatting

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -8,13 +8,18 @@ interface TaskCardProps {
 }
 
 const TaskCard: React.FC<TaskCardProps> = ({ title, dueDate, completedAtDate, assigneeName }) => {
+  let data = " ";
+      if (completedAtDate) {
+        data = "Completed on: " + completedAtDate.toDateString();
+      } else{
+        data = "Due on: " + dueDate.toDateString();
+      }
   return (
     <div className="p-4 bg-cyan-400 rounded-md shadow-md">
-      <h3 className="text-lg font-bold">{title}</h3>
-      {dueDate && <p>Due on: {dueDate.toLocaleDateString("en-CA")}</p>}
-      {completedAtDate && <p>Completed on: {completedAtDate.toLocaleDateString("en-CA")}</p>}
-      <p>Assignee: {assigneeName}</p>
-    </div>
+    <h3 className="text-lg font-bold">{title}</h3>
+    <p>{data}</p>
+    <p>Assignee: {assigneeName}</p>
+  </div>
   );
 };
 
