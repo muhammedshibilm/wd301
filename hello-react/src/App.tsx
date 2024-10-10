d6726e63e6e77402b450c895e0df3eee01db7661
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-10-10 14:50:10 +0530

    Refactor TaskCard component to update UI styling and improve date formatting

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,33 +1,59 @@
-import React from "react"
-import TaskCart from "./TaskCard"
-
+import React from "react";
+import TaskCart from "./TaskCard";
+import { Task } from "./TaskCard";
 
 function App() {
-   const pendingData =   [{ "title": "Build the website with static content", "dueDate": new Date('2023-12-31'), "assignee": "Rohit S" }, { "title": "Add a blog", "dueDate": new Date('2023-11-31'), "assignee": "Rohit M" }];
-  const doneData =  [{ "title": "Design the mockup", "completedAtDate": new Date('2024-12-31'), "assignee": "Rohit M" }, { "title": "Get the approval from principal", "completedAtDate": new Date('2024-11-31'), "assignee": "Ajay S" }]
-
+  const pendingData: Task[] = [
+    {
+      title: "Build the website with static content",
+      dueDate: new Date("2023-12-31"),
+      assigneeName: "Rohit S",
+    },
+    {
+      title: "Add a blog",
+      dueDate: new Date("2023-11-31"),
+      assigneeName: "Rohit M",
+    },
+  ];
+  const doneData: Task[] = [
+    {
+      title: "Design the mockup",
+      completedAtDate: new Date("2024-12-31"),
+      assigneeName: "Rohit M",
+    },
+    {
+      title: "Get the approval from principal",
+      completedAtDate: new Date("2024-11-31"),
+      assigneeName: "Ajay S",
+    },
+  ];
 
   return (
     <div className="p-4 space-y-4">
       <h1 className="text-xl font-bold">Smarter Tasks</h1>
-      <p><strong>Project: </strong>Graduation Final Year Project (Revamp College Website)</p>
+      <p>
+        <strong>Project: </strong>Graduation Final Year Project (Revamp College
+        Website)
+      </p>
       <div className="flex space-x-4">
         <div className="w-1/2 space-y-3 bg-cyan-500 p-4 rounded-md ">
           <h2 className="text-lg font-bold text-center">Pending Tasks</h2>
           {pendingData.map((task, index) => (
-            <TaskCart key={index} title={task.title} dueDate={task.dueDate} assigneeName={task.assignee} />
+            <TaskCart key={index} {...task} />
           ))}
-          <div className="bg-cyan-800 text-md font-semibold p-2 rounded-md">+ New task</div>
+          <div className="bg-cyan-800 text-md font-semibold p-2 rounded-md">
+            + New task
+          </div>
         </div>
         <div className="w-1/2 space-y-3 bg-cyan-500 p-4 rounded-md ">
           <h2 className="text-lg font-bold text-center">Done Tasks</h2>
           {doneData.map((task, index) => (
-            <TaskCart key={index} title={task.title} completedAtDate={task.completedAtDate} assigneeName={task.assignee} />
+            <TaskCart key={index} {...task} />
           ))}
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,25 +1,20 @@
 import React from "react";
 
-interface TaskCardProps {
+export interface Task {
   title: string;
   dueDate?: Date;
   completedAtDate?: Date;
   assigneeName: string;
 }
 
-const TaskCard: React.FC<TaskCardProps> = ({ title, dueDate, completedAtDate, assigneeName }) => {
-  let data = " ";
-      if (completedAtDate) {
-        data = "Completed on: " + completedAtDate.toDateString();
-      } else{
-        data = "Due on: " + dueDate.toDateString();
-      }
+const TaskCard: React.FC<Task> = ({ title, dueDate, completedAtDate, assigneeName }) => {
   return (
     <div className="p-4 bg-cyan-400 rounded-md shadow-md">
-    <h3 className="text-lg font-bold">{title}</h3>
-    <p>{data}</p>
-    <p>Assignee: {assigneeName}</p>
-  </div>
+      <h3 className="text-lg font-bold">{title}</h3>
+      {dueDate && <p>Due on: {dueDate.toLocaleDateString("en-CA")}</p>}
+      {completedAtDate && <p>Completed on: {completedAtDate.toLocaleDateString("en-CA")}</p>}
+      <p>Assignee: {assigneeName}</p>
+    </div>
   );
 };
 
